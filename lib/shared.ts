5340f595466cf2c767ff8766a7941f0bd21fe9fe
chiang <chiang@rightstack.net>
2026-04-05 23:28:07 +0900

    Rename docpler to Docpler in docs content

--- lib/shared.ts
+++ lib/shared.ts
@@ -1,4 +1,4 @@
-export const appName = 'docpler';
+export const appName = 'Docpler';
 export const docsRoute = '/docs';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
